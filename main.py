1ae469ea201c4e3d7c9fa06968f5a26e847a1a13
Vinay Kumar <vinayjangra.dev@gmail.com>
2025-07-28 10:05:25 +0530

    load data from csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 BACKGROUND_COLOR = "#B1DDC6"
 from tkinter import *
+import pandas as pd
 
 window = Tk()
 window.title("Echo Cards")
@@ -14,6 +15,9 @@ my_image = PhotoImage(file="./images/card_back.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=my_image)
 canvas.grid(row=0, column=0, columnspan=2)
+canvas_lang = canvas.create_text(400, 150, text="French",  font=("Arial", 40, "italic"), fill="black")
+canvas_word = canvas.create_text(400, 263, text="partir", font=("Arial", 60, "bold"), fill="black")
+
 
 wrong_img = PhotoImage(file="./images/wrong.png")
 wrong_btn = Button(image=wrong_img, highlightthickness=0)
@@ -25,6 +29,9 @@ right_btn = Button(image=right_img, highlightthickness=0)
 right_btn.grid(row=1, column=1)
 
 canvas.image = my_image
+
+words_data = pd.read_csv("./data/french_words.csv")
+print(words_data)
 
 
 window.mainloop()
